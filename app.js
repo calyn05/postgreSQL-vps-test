7939fb7f550d68f78e724ad705fc23e365203617
calyn05 <67522453+calyn05@users.noreply.github.com>
2023-06-29 16:02:15 +0300

    Actions test

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const path = require("path");
 const { Client } = require("pg");
 const Pool = require("pg").Pool;
 const env = require("dotenv");
-env.config();
+env.config({ path: path.join(__dirname, ".env") });
 
 const app = express();
 const port = 3000;
@@ -12,23 +13,27 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
+  user: process.env.USER_NAME,
+  host: process.env.HOST_NAME,
+  database: process.env.DATABASE_NAME,
+  password: process.env.USER_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
-app.get("/", (req, res) => {
-  pool.query("SELECT * FROM users", (err, result) => {
-    if (err) {
-      console.log(err);
-      res.send("Error");
-      return;
-    }
-    console.log(result.rows);
-    res.send(result.rows);
-  });
+app.get("/", async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query("SELECT * FROM users");
+    const results = { results: result ? result.rows : null };
+    res.send(results);
+    client.release();
+  } catch (err) {
+    res.send("Error " + err);
+  }
+});
+
+app.get("/test", (req, res) => {
+  res.send("Hello, world");
 });
 
 app.listen(port, () => {
